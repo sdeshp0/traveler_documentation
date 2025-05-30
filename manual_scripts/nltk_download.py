8f1d3f49062bbbbd7068a70407ec770b995ed5aa
snd21 <snd214@outlook.com>
2025-05-30 15:39:58 -0400

    move into main directory

--- manual_scripts/nltk_download.py
+++
@@ -1,3 +0,0 @@
-import nltk
-nltk.download('stopwords', download_dir='../nltk_resources')
-nltk.download('wordnet', download_dir='../nltk_resources')
---
+++ nltk_download.py
@@ -0,0 +1,3 @@
+import nltk
+nltk.download('stopwords', download_dir='nltk_resources')
+nltk.download('wordnet', download_dir='nltk_resources')
