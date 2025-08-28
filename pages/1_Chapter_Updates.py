22ca1a5e4f68db90706fb968d2adc4bfb3574590
snd21 <snd214@outlook.com>
2025-08-28 11:09:55 -0400

    updated chapter update file with text edits.

--- pages/1_Chapter_Updates.py
+++ pages/1_Chapter_Updates.py
@@ -4,12 +4,12 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 import warnings
 
-
-#Suppress FutureWarning messages
+# Suppress FutureWarning messages
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 st.set_page_config(layout='wide', page_title='Chapter Updates')
 st.logo('data/pokeball_logo.svg')
+
 
 def generate_chart(df):
     dates = [datetime.strptime(d, '%b %d, %Y') for d in df['Date*']]
@@ -52,11 +52,7 @@ else:
     df_updates = st.session_state['updates']
 
 chapter_cols = [c for c in df_updates.columns if 'Notes' not in c]
-chapter_notes = df_updates['Chapter Notes'].copy().dropna()
-chapter_notes.index.name='Ch'
 
-general_notes = df_updates['General Notes'].copy().dropna()
-general_notes.index.name=None
 
 st.markdown("<h1 style='text-align: center; color: grey;'> Traveler Chapter Update History </h1>", unsafe_allow_html=True)
 st.divider()
@@ -65,17 +61,27 @@ st.markdown("<p style='text-align: left; color: grey;'> This is the chapter upda
             "fanfiction story</p>", unsafe_allow_html=True)
 st.divider()
 
-st.subheader('Chapter Update Table')
-st.dataframe(df_updates[chapter_cols], width='stretch')
-st.divider()
-
 with st.expander('Display Chapter Update Chart'):
     st.pyplot(generate_chart(df_updates))
 st.divider()
 
-with st.expander('Display Chapter Notes'):
-    st.dataframe(chapter_notes, width='stretch')
-st.divider()
+st.subheader('Chapter Update Table')
+st.dataframe(df_updates[chapter_cols], use_container_width=True)
 
-with st.expander('Display General Notes'):
-    st.dataframe(general_notes, width='stretch')
+st.write('----- General Notes -----')
+st.write('*Chapter date is based on the earliest review posted on FFN.')
+
+st.write('**word counts based on https://wordcounter.net/. Counts may include section break text, '
+         'but exclude author\'s notes.')
+
+st.write('*** Note that the total word count might differ from what is shown on fanfiction.net. '
+         'This is because ffn uses a different methodology to count words that results in a roughly 2% higher word '
+         'count as compared to most word processors. See Reddit post by SteelbadgerMk2 in this thread for more '
+         'detail - https://www.reddit.com/r/FanFiction/comments/nkp1w2/word_count_on_ao3_and_ffnet/.')
+
+st.write('----- Chapter Specific Notes -----')
+st.write('The earliest review for chapter 46 was posted on Jan 2, 2019. Switched to next earliest review date of '
+         'Mar 24, 2019 because a same-day update does not make sense given the author\'s notes from '
+         'chapters 45 and 46.')
+
+st.divider()
